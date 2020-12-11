42691c4353ca1c01cca97e6b9ef03d886c040869
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-11 11:09:55 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Procedures from './Components/Procedures';
 import Contact from './Components/Contact';
+import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <Home />
-            <Procedures />
-            <Contact />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">HOME</a>
+                        </li>
+                        <li>
+                            <a href="/contact">CONTACT</a>
+                        </li>
+                        <li>
+                            <a href="/procedures">PROCEDURES</a>
+                        </li>
+                    </ul>
+                </nav>
+                <Route path="/" exact component={Home} />
+                <Route path="/procedures" component={Procedures} />
+                <Route path="/contact" component={Contact} />
+            </div>
+        </BrowserRouter>
     );
 }
 
