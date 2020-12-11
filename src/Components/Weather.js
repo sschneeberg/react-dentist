90a2ca3b57b0782d5bc2f64bf801a1ab8eae2faf
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-11 16:40:32 -0500

    Test out loading message

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,15 +6,18 @@ import WeatherResult from './WeatherResult';
 function Weather() {
     const [zip, setZip] = useState('');
     const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('ZIP', zip);
+        setLoading(true);
         const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=052f26926ae9784c2d677ca7bc5dec98`;
         axios
             .get(url)
             .then((result) => {
                 setWeather(result.data);
+                setLoading(false);
             })
             .catch((err) => console.log(err));
     };
@@ -23,7 +26,10 @@ function Weather() {
         <div className="Weather">
             <form onSubmit={handleSubmit}>
                 <h1>Find weather in your area in the United States</h1>
-                {weather ? <WeatherResult weather={weather} /> : null}
+                {loading ? <h3>Loading Results . . .</h3> : null}
+                {weather && !loading ? (
+                    <WeatherResult weather={weather} />
+                ) : null}
                 <label htmlFor="zip">Enter your Zip Code</label>
                 <input
                     type="text"
